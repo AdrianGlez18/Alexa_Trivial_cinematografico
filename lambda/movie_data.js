9ad82058e84d796d55343ed6c1ddc24784f112e9
alu0101016733 <60736352+alu0101016733@users.noreply.github.com>
2020-11-23 13:51:31 +0000

    Add files via upload

---
+++ lambda/movie_data.js
@@ -0,0 +1,23 @@
+module.exports = {
+    '0' : {
+        'id' : '0',
+        'title' : 'Matrix',
+        'year' : '1999',
+        'genre' : ['Ciencia ficción', 'Acción'],
+        'protagonists' : ['Keanu Reeves','Laurence Fishburne','Carrie-Anne Moss','Hugo Weaving','Joe Pantoliano','Marcus Chong','Paul Goddard','Gloria Foster']
+    },
+    '1' : {
+        'id' : '1',
+        'title' : 'Regreso al futuro',
+        'year' : '1985',
+        'genre' : ['Ciencia ficción', 'Aventuras','Comedia'],
+        'protagonists' : ['Michael Fox','Christopher Lloyd','Crispin Glover','Lea Thompson','Thomas Wilson']
+    },
+    '2' : {
+        'id' : '2',
+        'title' : 'Capitana Marvel',
+        'year' : '2019',
+        'genre' : ['Ciencia ficción', 'Acción','Superhéroes'],
+        'protagonists' : ['Brie Larson','Samuel Jackson','Ben Mendelsohn','Djimon Hounsou','Lee Pace','Lashana Lynch','Gemma Chan','Clark Gregg','Annette Bening','Jude Law']
+    }
+};
