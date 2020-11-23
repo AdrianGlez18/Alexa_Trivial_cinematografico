2eee86a3dc2bfc01b46dcb5f4736364bef5ee11f
AdrianGlez18 <adrianglezhdez18@gmail.com>
2020-11-23 12:14:14 +0000

    Creado desde la nube

--- lambda/index.js
+++ lambda/index.js
@@ -1,21 +1,13 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'
+        || Alexa.getRequestType(handlerInput.requestEnvelope) === 'GameModeIntent'
+        || Alexa.getRequestType(handlerInput.requestEnvelope) === 'AnswerQuestionIntent';
     },
     handle(handlerInput) {
-        initData();
-        const questionText = getQuestion();
-        currentStatus = 'Question';
-        const speakOutput = 'Hola! Vamos a jugar a ... ¿En qué año pasó? ... Tendrás que responder \
-        diciendo qué año corresponde con el hito al que hago referencia. ... Vamos a empezar! ...\
-        ' + questionText;
+        const speakOutput = 'Bienvenido a trivial cinematografico. Puedes hacerme preguntas sobre peliculas, o puedo ponerte a prueba haciéndotelas yo. Para empezar, hazme una pregunta o dime quiero jugar.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -24,177 +16,45 @@ const LaunchRequestHandler = {
     }
 };
 
-const AnswerIntentHandler = {
+const GameModeIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AnswerIntent';
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'InGameIntent')
     },
     handle(handlerInput) {
-        const AnswerValue = handlerInput.requestEnvelope.request.intent.slots.numberSlot.value;
-        let speakOutput = '';
-        if (currentStatus === 'Continue') {
-            speakOutput += 'Responde sí o no';
-        }
-        else {
-            if (AnswerValue === currentIndex.year) {
-                speakOutput += 'Respuesta correcta! ... ' + currentIndex.answer + '.';
-                hits++;
-            }
-            else  {
-                speakOutput += 'Respuesta incorrecta, la respuesta correcta es ' +  currentIndex.year+ ' porque ' + currentIndex.answer + '.';
-            }
-        }
-        currentIndex = null;
-        speakOutput += ' ... Continuamos? ';
-        currentStatus = 'Continue';
-        
-        if (exit) {
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .getResponse();
-        } 
-        else {
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .reprompt(speakOutput)
-                .getResponse();
-        }
-    }
-};
-
-const RepeatIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.RepeatIntent';
-    },
-    handle(handlerInput) {
-        let speakOutput = '';
-        if (currentStatus === 'Question') {
-            speakOutput += 'Repetimos! ... ' + getQuestion(false);
-        }
-        else if (currentStatus === 'Continue') {
-            speakOutput += 'Continuamos? ';
-        }
+        const speechText = "Has entrado al modo de juego. Te haré 5 preguntas, y veremos cuántas de ellas has logrado acertar. ¿Te animas?";
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
+            .speak(speechText)
+            //.reprompt()
             .getResponse();
     }
+    
 };
 
-
-const YesIntentHandler = {
+const InGameIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent';
+        return true;
     },
     handle(handlerInput) {
-        const speakOutput = getQuestion();
-        currentStatus = 'Question';
-
-
-        if (exit) {
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .withShouldEndSession(true)
-                .getResponse();
-        } 
-        else {
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .reprompt(speakOutput)
-                .getResponse();
-        }
-    }
-};
-
-const PendingIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PendingIntent';
-    },
-    handle(handlerInput) {
-        let speakOutput = '';
-        if (pending === null) {
-            if (currentIndex !== null && currentStatus === 'Question') {
-                speakOutput += 'Hemos dejado esta pregunta sin responder, la guardamos para después ... '; 
-                pending = currentIndex;
-            }
-            speakOutput += 'No tienes preguntas pendientes! ... Quieres continuar con una nueva pregunta?';
-            currentStatus = 'Continue';
-        }
-        else {
-            if (currentIndex !== null && currentStatus === 'Question') {
-                let tmpIndex = currentIndex;
-                currentIndex = pending;
-                pending = currentIndex;
-                speakOutput += 'Hemos dejado esta pregunta sin responder, la guardamos para después ... '; 
-            }
-            else {
-                currentIndex = pending;
-                pending = null;
-            }
-            
-            speakOutput += 'Vamos con la pregunta que teníamos pendiente! ... ' + getQuestion(false);
-            currentStatus = 'Question';
-        }
-
-
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
+        .speak("Iniciado InGameIntent")
+        .getResponse();
     }
-};
+}
 
-const NextIntentHandler = {
+const AnswerQuestionIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NextIntent';
+        return true;
     },
     handle(handlerInput) {
-        let speakOutput = '';
-        if (pending !== null) {
-            speakOutput = 'Alcanzaste el máximo de preguntas pendientes de responder, vamos a por ella de nuevo. ... ';
-            const tmpIndex = currentIndex;
-            currentIndex = pending;
-            pending = tmpIndex;
-            speakOutput += getQuestion(false);
-        }
-        else {
-            speakOutput = 'Guardamos esta pregunta para después, vamos con la siguiente! ... ';
-            pending = currentIndex;
-            speakOutput += getQuestion();
-        }
-        currentStatus = 'Question';
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
+        .speak("Iniciado AnswerQuestionIntent")
+        .getResponse();
     }
-};
+}
 
-const ClueIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ClueIntent';
-    },
-    handle(handlerInput) {
-        let speakOutput = '';
-        if (currentStatus === 'Question') {
-            speakOutput += 'Ahí va una pista! ... ' + currentIndex.clue + '. ... Te vuelvo a repetir la pregunta. ... ' + getQuestion(false);
-        }
-        else if (currentStatus === 'Continue') {
-            speakOutput += 'Responde Sí o No.';
-        }
-        
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+
 
 const HelpIntentHandler = {
     canHandle(handlerInput) {
@@ -202,9 +62,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'El juego consiste en que te iré haciendo preguntas y tendrás que acertar el año \
-        correcto, pero si no sabes la respuesta puedes decirme que pase a la \
-        siguiente y así tendrás tiempo de pensar la respuesta. Puedes tener hasta una pregunta pendiente de responder.';
+        const speakOutput = 'Si tienes alguna duda, no dudes en preguntarme.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -217,11 +75,11 @@ const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NoIntent');
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'Has conseguido acertar ' + hits + ' de ' + count + ' preguntas. ... Hasta luego!';
+        const speakOutput = 'Gracias por utilizar trivial cinematográfico, una skill desarrollada por Adrian y Thaddeus. Esperamos volver a oirte pronto.';
+
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
@@ -238,7 +96,7 @@ const FallbackIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Lo siento, no entiendo lo que me dices. Por favor inténtalo otra vez.';
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -290,7 +148,7 @@ const ErrorHandler = {
         return true;
     },
     handle(handlerInput, error) {
-        const speakOutput = 'Lo siento, tuve problemas para hacer lo que me pediste. Inténtalo de nuevo.';
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
         console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
 
         return handlerInput.responseBuilder
@@ -300,86 +158,6 @@ const ErrorHandler = {
     }
 };
 
-
-
-let questionsList, currentIndex, count, hits, pending, currentStatus, exit;
-function initData() {
-    //questionsList = require('./question-list');
-    questionsList = {
-        '0' : {
-            'id' : '0',
-            'question' : 'Ivan Sutherland desarrolló el Sketchpad',
-            'year' : '1963',
-            'answer' : 'En 1963, Ivan Sutherland desarrolló el Sketchpad. El Sketchpad fue el primer programa informático que permitió la manipulación directa de objetos gráficos, \
-            pionero en la interacción persona-ordenador y predecesor de los programas de diseño asistido por ordenador',
-            'clue' : 'El 28 de agosto de ese año, Martin Luther King jr. pronuncia el discurso: I have a dream'
-        },
-        '1' : {
-            'id' : '1',
-            'question' : 'Douglas Engelbart diseña el primer mouse',
-            'year' : '1963',
-            'answer' : 'En 1963, Douglas Engelbart diseña el primer ratón. El ratón es un dispositivo apuntador utilizado para facilitar el manejo de un entorno gráfico en una computadora',
-            'clue' : 'El 22 de noviembre de ese año el presidente de los Estados Unidos de América, John F. Kennedy es asesinado en Dallas'
-        },
-        '2' : {
-            'id' : '2',
-            'question' : 'Se desarrolla en el MIT’s Lincoln Labs, el AMBIT G',
-            'year' : '1968',
-            'answer' : 'En 1968 se el MIT’s Lincoln Labs desarrolla el AMBIT G, que es un sistema que incluyó representaciones con iconos y su selección, reconocimiento de gestos y menús dinámicos de ayuda',
-            'clue' : 'El 12 de octubre de ese año se inauguran los juegos olímpicos en la Ciudad de México'
-        },
-        '3' : {
-            'id' : '3',
-            'question' : 'Xerox Star sale a la venta',
-            'year' : '1981',
-            'answer' : 'En 1981, se pone a la venta Xerox Star, siendo este el primer sistema comercial con un uso extenso de la manipulación directa',
-            'clue' : 'El 5 de junio de ese año se publica el primer informe sobre el VIH causante del sida'
-        }
-    };
-    currentIndex = null;
-    count = 0;
-    hits = 0;
-    pending = null;
-    currentStatus = null;
-    exit = false;
-}
-
-
-
-function getRandomItem(obj) {
-    if (Object.keys(obj).length === 0) {
-        return null;
-    }
-    currentIndex =  obj[Object.keys(obj)[Math.floor(Math.random()*Object.keys(obj).length)]];
-    return currentIndex;
-}
-
-function getQuestion(random = true) {
-    let speechText = '';
-    if (random) {
-        speechText = getRandomItem(questionsList);
-        if (currentIndex === null && pending === null) {
-            const speakOutput = 'Ya respondiste todas las preguntas! ... Has conseguido acertar ' + hits + ' de ' + count + ' preguntas. ... Hasta luego!';
-            exit = true;
-            return speakOutput;
-        }
-        else if (currentIndex === null) {
-            return 'Ya no te quedan más preguntas nuevas, pero sí te queda una pendiente, vamos con ella. ... ' + '¿En qué año ' + speechText.question + '? ';
-        }
-        delete questionsList[currentIndex.id];
-        count++;
-    }
-    else {
-        speechText = currentIndex;
-    }
-    const speakOutput = '¿En qué año ' + speechText.question + '? ';
-    return speakOutput
-}
-
-
-
-
-
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -388,13 +166,10 @@ function getQuestion(random = true) {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        AnswerIntentHandler,
-        RepeatIntentHandler,
-        NextIntentHandler,
-        ClueIntentHandler,
+        GameModeIntentHandler,
+        InGameIntentHandler,
+        AnswerQuestionIntentHandler,
         HelpIntentHandler,
-        YesIntentHandler,
-        PendingIntentHandler,
         CancelAndStopIntentHandler,
         FallbackIntentHandler,
         SessionEndedRequestHandler,
